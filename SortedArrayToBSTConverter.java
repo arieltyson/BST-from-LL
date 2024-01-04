e8a681c7fa751e8beb16cf18fc4f0dbdceef3d7d
Ariel Tyson <ariel30190@hotmail.com>
2024-01-04 00:55:39 -0800

    Create a class for converting a sorted array to a binary search tree

---
+++ SortedArrayToBSTConverter.java
@@ -0,0 +1,21 @@
+public class SortedArrayToBSTConverter {
+
+    public TreeNode convert(int[] nums) {
+        if (nums.length == 0) {
+            return null;
+        }
+        return constructBSTRecursive(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode constructBSTRecursive(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        int mid = left + (right - left) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = constructBSTRecursive(nums, left, mid - 1);
+        node.right = constructBSTRecursive(nums, mid + 1, right);
+        return node;
+    }
+}
